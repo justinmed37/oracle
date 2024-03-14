f134c14cb1e03ad3d30121f2d9a3e5bbaf5645a9
jmad <justinmed37@gmail.com>
2024-03-14 15:33:23 -0400

    Create projectX/tfstate bucket

--- infrastructure/bootstrap/main.tf
+++ infrastructure/bootstrap/main.tf
@@ -1,10 +1,20 @@
-variable tenancy_ocid {
-    default = "ocid1.tenancy.oc1..aaaaaaaaiutrjzaumegnunzwoqhngqcwnewh2ptjd4jqhqk6ovs47uqlso3a"
+variable "tenancy_ocid" {
+  default = "ocid1.tenancy.oc1..aaaaaaaaiutrjzaumegnunzwoqhngqcwnewh2ptjd4jqhqk6ovs47uqlso3a"
 }
 
-resource oci_identity_compartment projectX {
-    #Required
-    compartment_id = var.tenancy_ocid
-    description = "projectX compartment"
-    name = "projectX"
+resource "oci_identity_compartment" "projectX" {
+  # Required
+  compartment_id = var.tenancy_ocid
+  description    = "projectX compartment"
+  name           = "projectX"
+}
+
+resource "oci_objectstorage_bucket" "tfstate" {
+  # Required
+  compartment_id = resource.oci_identity_compartment.projectX.id
+  name           = "tfstate"
+  namespace      = "idbjyurhyjpo"
+
+  # Optional
+  versioning = "Enabled"
 }
--- infrastructure/bootstrap/provider.tf
+++ infrastructure/bootstrap/provider.tf
@@ -1,6 +1,6 @@
 variable "config_file_profile" {}
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
+  tenancy_ocid        = var.tenancy_ocid
   config_file_profile = "DEFAULT"
 }
