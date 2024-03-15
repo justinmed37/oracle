74a452ffe38f8437f96da09ce4a17e37b70cfbb9
jmad <justinmed37@gmail.com>
2024-03-14 20:00:14 -0400

    TEST

--- infrastructure/bootstrap/provider.tf
+++ infrastructure/bootstrap/provider.tf
@@ -4,3 +4,12 @@ provider "oci" {
   tenancy_ocid        = var.tenancy_ocid
   config_file_profile = "DEFAULT"
 }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "3.74.0"
+#     }
+#   }
+# }
