f60640d0d317301a1f161180d5eb8a67841dc310
jmad <justinmed37@gmail.com>
2024-03-16 14:10:45 -0400

    oracle provider

--- infrastructure/bootstrap/provider.tf
+++ infrastructure/bootstrap/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 1.7.4"
+  required_providers {
+    oci = {
+      version = ">= 4.44.0 "
+      source = "oracle/oci"
+    }
+  }
+}
 # provider "oci" {
 #   tenancy_ocid        = var.tenancy_ocid
 #   config_file_profile = "DEFAULT"
