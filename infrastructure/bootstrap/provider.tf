4fe5126debace91d154ce5d3e618eecf11fc287c
justinmed37 <justinmed37@gmail.com>
2024-03-16 14:43:11 -0400

    test workflow (#3)
    Integrate GitHub Actions with Terraform and OCI

--- infrastructure/bootstrap/provider.tf
+++ infrastructure/bootstrap/provider.tf
@@ -1,6 +1,22 @@
-variable "config_file_profile" {}
-
-provider "oci" {
-  tenancy_ocid        = var.tenancy_ocid
-  config_file_profile = "DEFAULT"
+terraform {
+  required_version = ">= 1.7.4"
+  required_providers {
+    oci = {
+      version = ">= 4.44.0 "
+      source = "oracle/oci"
+    }
+  }
 }
+# provider "oci" {
+#   tenancy_ocid        = var.tenancy_ocid
+#   config_file_profile = "DEFAULT"
+# }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "3.74.0"
+#     }
+#   }
+# }
