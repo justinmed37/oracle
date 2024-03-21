0a6bb63328ff0db5cece309d8e1f4b588cd37971
jmad <justinmed37@gmail.com>
2024-03-21 19:58:03 -0400

    cleanup

--- infrastructure/bootstrap/main.tf
+++ infrastructure/bootstrap/main.tf
@@ -27,5 +27,5 @@ resource "oci_objectstorage_bucket" "generic_bu" {
   compartment_id = resource.oci_identity_compartment.generic_bu.id
   name           = "generic_bu_tfstate"
   namespace      = "idbjyurhyjpo"
-  versioning = "Enabled"
+  versioning     = "Enabled"
 }
