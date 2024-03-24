be89743b8429e2d588db1bf9dc0bcb3176fac60d
jmad <justinmed37@gmail.com>
2024-03-24 12:58:27 -0400

    ssl

--- container/frontend/main.py
+++ container/frontend/main.py
@@ -9,7 +9,7 @@ import ssl
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain('/app/frontend/ca_bundle.pem', keyfile='/app/frontend/private.pem')
-app = FastAPI()
+server = FastAPI()
 
 # here we use our custom page decorator directly and just put the content creation into a separate function
 @ui.page('/')
@@ -22,7 +22,7 @@ def index_page() -> None:
 pages.create()
 app.add_static_files('/.well-known/pki-validation', 'pki')
 
-ui.run_with(app)
+ui.run_with(server)
 # ui.run(
 #     title='OCI Cloud Infrastructure Demo',
 #     port=int(os.environ.get("HTTP_PORT")),
@@ -32,4 +32,4 @@ ui.run_with(app)
 #     # ssl_keyfile="/app/frontend/private.pem",
 # )
 
-uvicorn.run(app, host='0.0.0.0', port=443)
+uvicorn.run(server, host='0.0.0.0', port=443)
