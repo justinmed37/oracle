7b4630806cc09d53798650c4e7d09bbd0f2e24d6
jmad <justinmed37@gmail.com>
2024-03-24 12:39:30 -0400

    certs

--- container/frontend/main.py
+++ container/frontend/main.py
@@ -3,6 +3,10 @@ import home_page
 import theme
 import os
 from nicegui import app, ui
+import ssl
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain('/app/frontend/ca_bundle.pem', keyfile='/app/frontend/private.pem')
 
 
 # here we use our custom page decorator directly and just put the content creation into a separate function
@@ -20,6 +24,7 @@ ui.run(
     title='OCI Cloud Infrastructure Demo',
     port=int(os.environ.get("HTTP_PORT")),
     uvicorn_logging_level="info",
-    ssl_certfile="/app/frontend/ca_bundle.pem",
-    ssl_keyfile="/app/frontend/private.pem",
+    kwargs={"ssl_context": ssl_context}
+    # ssl_certfile="/app/frontend/ca_bundle.pem",
+    # ssl_keyfile="/app/frontend/private.pem",
 )
