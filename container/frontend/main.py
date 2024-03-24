363865d7cf5c09710d16c448d882001521a8462b
jmad <justinmed37@gmail.com>
2024-03-24 13:25:16 -0400

    container

--- container/frontend/main.py
+++ container/frontend/main.py
@@ -2,14 +2,13 @@ import pages
 import home_page
 import theme
 import os
-import uvicorn
 from fastapi import FastAPI
-from nicegui import app, ui
+from nicegui import ui
 import ssl
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain('/app/frontend/ca_bundle.pem', keyfile='/app/frontend/private.pem')
-server = FastAPI()
+# server = FastAPI()
 
 # here we use our custom page decorator directly and just put the content creation into a separate function
 @ui.page('/')
@@ -20,21 +19,20 @@ def index_page() -> None:
 
 # this call shows that you can also move the whole page creation into a separate file
 pages.create()
-app.add_static_files('/.well-known/pki-validation', 'pki')
 
-ui.run_with(server)
-# ui.run(
-#     title='OCI Cloud Infrastructure Demo',
-#     port=int(os.environ.get("HTTP_PORT")),
-#     uvicorn_logging_level="info",
-#     ssl=ssl_context
-#     # ssl_certfile="/app/frontend/ca_bundle.pem",
-#     # ssl_keyfile="/app/frontend/private.pem",
-# )
+# ui.run_with(server)
+ui.run(
+    title='OCI Cloud Infrastructure Demo',
+    port=int(os.environ.get("HTTP_PORT")),
+    uvicorn_logging_level="info",
+    ssl=ssl_context
+    # ssl_certfile="/app/frontend/ca_bundle.pem",
+    # ssl_keyfile="/app/frontend/private.pem",
+)
 
 # uvicorn.run(server, port=int(os.environ.get("HTTP_PORT")), ssl=ssl_context)
-uvicorn.run(server,
-    port=int(os.environ.get("HTTP_PORT")),
-    ssl_certfile="/app/frontend/ca_bundle.pem",
-    ssl_keyfile="/app/frontend/private.pem"
-)
+# uvicorn.run(server,
+#     port=int(os.environ.get("HTTP_PORT")),
+#     ssl_certfile="/app/frontend/ca_bundle.pem",
+#     ssl_keyfile="/app/frontend/private.pem"
+# )
