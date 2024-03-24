88c59516fbfc690ce62a9ef09b48a6011e9fad14
jmad <justinmed37@gmail.com>
2024-03-24 12:52:39 -0400

    ssl

--- container/frontend/main.py
+++ container/frontend/main.py
@@ -2,12 +2,14 @@ import pages
 import home_page
 import theme
 import os
+import uvicorn
+from fastapi import FastAPI
 from nicegui import app, ui
 import ssl
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 ssl_context.load_cert_chain('/app/frontend/ca_bundle.pem', keyfile='/app/frontend/private.pem')
-
+app = FastAPI()
 
 # here we use our custom page decorator directly and just put the content creation into a separate function
 @ui.page('/')
@@ -20,11 +22,14 @@ def index_page() -> None:
 pages.create()
 app.add_static_files('/.well-known/pki-validation', 'pki')
 
-ui.run(
-    title='OCI Cloud Infrastructure Demo',
-    port=int(os.environ.get("HTTP_PORT")),
-    uvicorn_logging_level="info",
-    kwargs={"ssl": ssl_context}
-    # ssl_certfile="/app/frontend/ca_bundle.pem",
-    # ssl_keyfile="/app/frontend/private.pem",
-)
+ui.run_with(app)
+# ui.run(
+#     title='OCI Cloud Infrastructure Demo',
+#     port=int(os.environ.get("HTTP_PORT")),
+#     uvicorn_logging_level="info",
+#     ssl=ssl_context
+#     # ssl_certfile="/app/frontend/ca_bundle.pem",
+#     # ssl_keyfile="/app/frontend/private.pem",
+# )
+
+uvicorn.run(app, host='0.0.0.0', port=443)
